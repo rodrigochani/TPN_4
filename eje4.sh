6e127e710ec534ec23e729c01896cc786dd13c38
rodrigochani <rodrigochani@live.com.ar>
2024-05-10 16:00:14 -0300

    Commit del cuarto ejercicio.

---
+++ eje4.sh
@@ -0,0 +1,23 @@
+################################ Maestría en Aplicaciones de Información Espacial ################################
+
+################################ Introducción a las técnicas inteligentes de resolución de problemas de planificación, secuenciación y ejecución ################################
+
+################################ Trabajo práctico Nº: 4 ################################
+
+################################ Ejercicio 4: Haga un programa que sume dos variables que usted ingresó como variables de entrada. ################################
+
+# Pedir al usuario que ingrese el primer número
+echo "Por favor, introduce el primer número:"
+read num1
+
+# Pedir al usuario que ingrese el segundo número
+echo "Por favor, introduce el segundo número:"
+read num2
+
+# Sumar los números y almacenar el resultado
+suma=$((num1 + num2))
+
+# Imprimir el resultado
+echo "La suma entre $num1 y $num2 es $suma."
+
+# Modo de ejecución: abrir en terminal Linux (Bash) con "./eje3.sh", dentro de la carpeta contenedora del archivo. #
