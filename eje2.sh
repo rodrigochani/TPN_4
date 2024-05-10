6eb6a0151cad1ed6aa1381e4b70225f872d49b02
rodrigochani <rodrigochani@live.com.ar>
2024-05-10 15:42:54 -0300

    Commit del segundo ejercicio.

---
+++ eje2.sh
@@ -0,0 +1,11 @@
+################################ Maestría en Aplicaciones de Información Espacial ################################
+
+################################ Introducción a las técnicas inteligentes de resolución de problemas de planificación, secuenciación y ejecución ################################
+
+################################ Trabajo práctico Nº: 4 ################################
+
+################################ Ejercicio 2: Imprima su nombre de usuario en su terminal. Rercordar que la variable de entorno $USER aloja el nombre del usuario actual. ################################
+
+echo "Hola $USER"
+
+# Modo de ejecución: abrir en terminal Linux (Bash) con "./eje2.sh", dentro de la carpeta contenedora del archivo. #
