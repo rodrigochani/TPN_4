c229486bc5aa03311ff94df7446b62273efd6972
rodrigochani <rodrigochani@live.com.ar>
2024-05-10 15:39:47 -0300

    Commit del primer ejercicio.

---
+++ eje1.sh
@@ -0,0 +1,11 @@
+################################ Maestría en Aplicaciones de Información Espacial ################################
+
+################################ Introducción a las técnicas inteligentes de resolución de problemas de planificación, secuenciación y ejecución ################################
+
+################################ Trabajo práctico Nº: 4 ################################
+
+################################ Ejercicio 1: Imprima “Hola mundo!” en su terminal, mediante un script. ################################
+
+echo "Hola mundo"
+
+# Modo de ejecución: abrir en terminal Linux (Bash) con "./eje1.sh", dentro de la carpeta contenedora del archivo. #
