e0cb6ee05b7f2c54a1c5a3f6c014ac45db4e966a
rodrigochani <rodrigochani@live.com.ar>
2024-05-10 20:26:48 -0300

    Commit del octavo ejercicio.

---
+++ eje8.sh
@@ -0,0 +1,13 @@
+################################ Maestría en Aplicaciones de Información Espacial ################################
+
+################################ Introducción a las técnicas inteligentes de resolución de problemas de planificación, secuenciación y ejecución ################################
+
+################################ Trabajo práctico Nº: 4 ################################
+
+################################ Ejercicio 8: Imprima el modelo de su procesador de su máquina mediante un scrip que lo muestre. ################################
+
+# Imprime el modelo del procesador usando lscpu (list CPU)
+
+lscpu | grep "Model name"
+
+# Modo de ejecución: abrir en terminal Linux (Bash) con "./eje8.sh", dentro de la carpeta contenedora del archivo. #
