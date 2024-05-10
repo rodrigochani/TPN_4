c35233f906361804f45bfd52425f6c67ad50f9f3
rodrigochani <rodrigochani@live.com.ar>
2024-05-10 15:47:33 -0300

    Commit del tercer ejercicio.

---
+++ eje3.sh
@@ -0,0 +1,20 @@
+################################ Maestría en Aplicaciones de Información Espacial ################################
+
+################################ Introducción a las técnicas inteligentes de resolución de problemas de planificación, secuenciación y ejecución ################################
+
+################################ Trabajo práctico Nº: 4 ################################
+
+################################ Ejercicio 3: Haga un programa en bash, donde usted debe pasar los argumentos de su nombre y edad y el programa los imprime en pantalla. ################################
+
+# Pedir al usuario que ingrese su nombre
+echo "Por favor, introduce tu nombre:"
+read nombre
+
+# Pedir al usuario que ingrese su edad
+echo "Por favor, introduce tu edad:"
+read edad
+
+# Imprimir los valores en pantalla
+echo "Hola, mi nombre es $nombre y tengo $edad años."
+
+# Modo de ejecución: abrir en terminal Linux (Bash) con "./eje3.sh", dentro de la carpeta contenedora del archivo. #
